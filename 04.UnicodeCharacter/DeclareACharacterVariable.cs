d2a47d54f2b0e1db70db50955e0289ada2c01e69
iliansimeonov <ilianb.simeonov@gmail.com>
2015-04-15 11:43:04 +0300

    Revert "Revert "Almost done""
    This reverts commit 379d8aefd1687bf382cc15fbce3c15f11fd9a5cc.

---
+++ 04.UnicodeCharacter/DeclareACharacterVariable.cs
@@ -0,0 +1,19 @@
+﻿//Problem 4. Unicode Character
+
+//Declare a character variable and assign it with the symbol that has Unicode code 42 (decimal)
+//using the \u00XX syntax, and then print it.
+
+using System;
+class DeclareACharacterVariable
+{
+    static void Main()
+    {
+        int decValue = 42;
+        string hexValue = decValue.ToString();
+        char unicodeChar = Convert.ToChar(decValue);
+        {
+            Console.WriteLine("The hex representation of {0} is {1}, and the char for {1} is {2} ", decValue, hexValue, unicodeChar);
+            Console.ReadLine();
+        }
+    }
+}
